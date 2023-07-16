deed9a313fe036fa8d90d2e4b1589a08168d9e59
Mumtaz <fatim25m@mtholyoke.edu>
2023-07-16 19:02:51 -0400

    initialize react router

---
+++ creatorverse/src/main.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+
+import './index.css'
+
+//import different components
+import NotFound from "../routes/NotFound"
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <Routes>
+        <Route path = "/" element = {<App/>}>
+        </Route>
+        <Route path = "*" element = {<NotFound/>} />
+      </Routes>
+    </BrowserRouter>
+  </React.StrictMode>,
+)
